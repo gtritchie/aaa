925f840b66abd6e049ee17b0f6094305203dfab9
Gary Ritchie <gary@rstudio.com>
2017-07-25 10:43:23 -0700

    commit

---
+++ colorful.R
@@ -0,0 +1,5 @@
+colorful <- function() {
+  for (color in 0:255){
+    cat("\033[48;5;", color, "m", color, "\n", sep="")
+  }
+}
--- keepbusy.R
+++ keepbusy.R
@@ -1,9 +1,11 @@
 keepbusy <- function() {
   a <- 0
   while (TRUE) {
-    Sys.sleep(3)
+    Sys.sleep(1)
     print(a)
     a <- a + 1
-
-    }
+    Sys.sleep(2)
+    print(a)
+    a <- a + 1
+  }
 }
