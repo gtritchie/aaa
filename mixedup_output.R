a54001ba59603dde77db2e3df0ea1a2eb3f8163e
Gary <gary@rstudio.com>
2019-05-11 11:36:33 -0700

    stuff

---
+++ mixedup_output.R
@@ -0,0 +1,29 @@
+library(crayon)
+library(progress)
+
+show_progress <- function(format, size, width = 80) {
+  pb <- progress_bar$new(total = size, format = format, width = width)
+  for (i in 1:size) {
+    pb$tick()
+    Sys.sleep(1 / size)
+  }
+}
+
+# Works with colors and 1 ms updates
+foo1 <- function() {
+  show_progress(":bar " %+% green(":rate"), 1000)
+}
+
+# Works without colors and .1 ms updates
+foo2 <- function() {
+  show_progress(":bar " %+% ":rate", 10000)
+}
+
+# Doesn't work with colors and .1 ms updates
+foo3 <- function() {
+  show_progress(":bar " %+% green(":rate"), 10000)
+}
+
+say <- function() {
+  cat("gary")
+}
