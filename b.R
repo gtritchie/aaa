f18fa230b8ccf9a90bcc5c35482d4e111ce180e9
Gary Ritchie <gary@rstudio.com>
2021-02-03 11:37:54 -0800

    And so it goes

--- b.R
+++ b.R
@@ -1,13 +1,3 @@
-# here's a comment to test some VCS stuff.
-# another comment
-b <- 25
-c <- b
-c <-  
-  
-# zoopy dog cat mouse
-# once up a time
-# no comment!
-  
 zoopy <- function() {
    cat("mouse")
    floopy("day")
