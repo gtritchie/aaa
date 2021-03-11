fc50d78bb52d8de07a77f8ba9d63f916ca89530e
Gary Ritchie <gary@rstudio.com>
2021-03-11 14:43:57 -0800

    languages test file

---
+++ languages.R
@@ -0,0 +1,12 @@
+# Words in various languages, with correct on left, incorrect on right
+
+# US English
+# mountane mountain
+# todey today
+
+# Korean
+# 요세   요새
+# 설겆이   설거지
+# 오랫만에   오랜만에
+# 바꼈다   바뀌었다
+# 어의없다   어이없다
