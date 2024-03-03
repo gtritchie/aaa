8b419388e2611f64ea409eb76cf8dc4a01104ef6
Gary Ritchie <gary@rstudio.com>
2024-03-03 09:39:42 -0700

    https://github.com/rstudio/rstudio/issues/14297

---
+++ custom-markers.R
@@ -0,0 +1,29 @@
+# from https://github.com/rstudio/rstudio/issues/14297
+#
+# Source this, then source the temp file it generates
+
+tmpfile <- tempfile(fileext = ".R")
+
+writeLines(con = tmpfile,
+           'foo <- cli::bg_br_white(cli::col_red("I am red!"))
+bar <- cli::bg_br_yellow(cli::col_cyan("I am cyan!"))
+
+markers <- list(
+  list(
+    type = "error",
+    file = tmpfile,
+    line = 6,
+    column = 1,
+    message = foo),
+  list(
+    type = "info", 
+    file = tmpfile,
+    line = 12,
+    column = 1,
+    message = bar))
+
+rstudioapi::sourceMarkers("Test Name", markers)
+'
+)
+
+file.edit(tmpfile)
