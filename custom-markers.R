ee411c770e6163558bbd93c594bb5520ca5d7b8a
Gary Ritchie <gary@rstudio.com>
2024-04-09 13:51:37 -0600

    testing commits

--- custom-markers.R
+++ custom-markers.R
@@ -2,8 +2,8 @@
 #
 # Source this, then source the temp file it generates
 
-tmpfile <- tempfile(fileext = ".R")
-
+# tmpfile <- tempfile(fileext = ".R")
+tmpfile <- '/Users/garyritchie/scratch/markers.R'
 writeLines(con = tmpfile,
            'foo <- cli::bg_br_white(cli::col_red("I am red!"))
 bar <- cli::bg_br_yellow(cli::col_cyan("I am cyan!"))
